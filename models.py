c7bc8d6ee1fdbb7927a696319954c3a67bb5d4c8
zjerryxie <96810007+zjerryxie@users.noreply.github.com>
2025-08-16 16:55:19 -0400

    Update models.py

--- models.py
+++ models.py
@@ -21,13 +21,11 @@ class Client(db.Model):
         return cipher.decrypt(self.ssn_encrypted.encode()).decode()
 
 class AuditLog(db.Model):
+     __tablename__ = "audit_logs"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     action = db.Column(db.String(100))  # E.g., "Viewed Client SSN"
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-class Client(db.Model):
-
 
 class TaxDocument(db.Model):
     id = db.Column(db.Integer, primary_key=True)
