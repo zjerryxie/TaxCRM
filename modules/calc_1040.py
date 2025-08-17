25153707df5eda43e832a0b41954addcbfe4bfca
zjerryxie <96810007+zjerryxie@users.noreply.github.com>
2025-08-17 11:50:22 -0400

    Create calc_1040.py

---
+++ modules/calc_1040.py
@@ -0,0 +1,14 @@
+def standard_deduction(status, age_flags):
+  pass
+
+def agi(w2_wages, interest, dividends):
+  pass
+
+def taxable_income(agi, standard_deduction):
+  pass
+
+def tax_liability_2024(taxable_income) (rate table):
+  pass
+
+def refund_or_balance(liability, withholding, credits):
+  pass
